8ec63f7128df3cc4562c5370dd297f0112ae930d
Tabby Dev <88893827+Tabby-Labs@users.noreply.github.com>
2025-06-30 23:26:16 +0800

    fix(client): wrong ms calculation, play anim perfectly

--- client.lua
+++ client.lua
@@ -5,9 +5,15 @@ local config = require 'config'
 RegisterNetEvent('stomachache:client:StomachEffect', function()
     if not qbx:GetPlayerData().metadata.stomachAche then return end
 
+    local ped = cache.ped
+
+    if not IsEntityPlayingAnim(ped, config.Animation.dict, config.Animation.anim, 3) then
+        lib.playAnim(ped, config.Animation.dict, config.Animation.anim, 1.0, 1.0, -1, 1, 0, false, false, false) 
+    end
+
     if lib.progressBar({
-        duration = config.Animation.time * 60000,
-        label = config.label,
+        duration = config.Animation.time * 1000,
+        label = config.Label,
         useWhileDead = false,
         canCancel = false,
         disable = {
@@ -16,11 +22,9 @@ RegisterNetEvent('stomachache:client:StomachEffect', function()
             combat = true,
             sprint = true
         },
-        anim = {
-            dict = config.Animation.dict,
-            clip = config.Animation.anim
-        },
-    }) then print('Do stuff when complete') else return end
+    }) then
+        TaskPlayAnim(ped, config.Animation.dict, 'exit', 1.0, 8.0, -1, 1, -1, false, false, false)
+    end
 end)
 
 -- callback
